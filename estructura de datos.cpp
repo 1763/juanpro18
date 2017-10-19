02826acbb0a91be0a087b7437f90af541999cc14
1763 <32943557+1763@users.noreply.github.com>
2017-10-19 17:21:10 -0500

    Add files via upload

---
+++ estructura de datos.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+string name;
+
+int main()
+{
+    cout << "Estructura de datos" << endl;
+    cout << "-------------------"<<endl;
+    cout << "Ingrese su nombre: "; cin >> name;
+    cout << "Hola, " << name << endl;
+    system ("pause");
+    return 0;
+}
